402721ba3c83246195af58ba17074e03f7f5b171
Alex Elízaga <alexelizaga@icloud.com>
2025-10-14 08:07:19 +0200

    Add useEffect-style hook for pages and components

--- public/src/pages/ReactDemo/ReactDemo.page.js
+++ public/src/pages/ReactDemo/ReactDemo.page.js
@@ -2,13 +2,25 @@ import { registerPage } from '../../ui/definePage.js';
 
 console.log('[react-page] registrado');
 
-registerPage('react-page', (vm, { html, state, scope }) => {
+registerPage('react-page', (vm, { html, state, scope, effect }) => {
   const [getClicks, setClicks] = state(0,  { reRender: false });
 
   scope.getClicks = getClicks;
 
   scope.increment = () => setClicks(c => c + 1);
   scope.reset = () => setClicks(0);
+
+  effect(() => {
+    const stop = scope.$watch(getClicks, (next, prev) => {
+      if (next !== prev) {
+        console.log('[react-page] clicks changed →', next);
+      }
+    });
+
+    return () => {
+      stop();
+    };
+  }, []);
 
   return html`
     <div class="py-4">
